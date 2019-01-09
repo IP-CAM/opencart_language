f86090c0228b8a50bd18a30a78d6ad9750f63bb1
Emilio J. Padrón González <emilioj@udc.gal>
2019-01-09 17:59:41 +0100

    [Galician] Add files gl.php and gl.png

---
+++ galician_pv/upload/catalog/language/gl/gl.php
@@ -0,0 +1,74 @@
+<?php
+// Locale
+$_['code']                  = 'gl';
+$_['direction']             = 'ltr';
+$_['date_format_short']     = 'd M';
+$_['date_format_long']      = 'l dS F Y';
+$_['time_format']           = 'h:i:s A';
+$_['datetime_format']       = 'd/m/Y H:i:s';
+$_['decimal_point']         = ',';
+$_['thousand_point']        = '.';
+
+// Text
+$_['text_home']             = '<i class="fa fa-home"></i>';
+$_['text_yes']              = 'Si';
+$_['text_no']               = 'Non';
+$_['text_none']             = ' --- Ningún --- ';
+$_['text_select']           = ' --- Seleccionar --- ';
+$_['text_all_zones']        = 'Todas as zonas';
+$_['text_pagination']       = 'Mostrando %d a %d de %d (%d Páxinas)';
+$_['text_loading']          = 'Cargando...';
+$_['text_no_results']       = 'Sen resultados!';
+
+// Buttons
+$_['button_address_add']    = 'Engadir enderezo';
+$_['button_back']           = 'Regresar';
+$_['button_continue']       = 'Continuar';
+$_['button_cart']           = 'Comprar';
+$_['button_cancel']         = 'Cancelar';
+$_['button_compare']        = 'Comparar este producto';
+$_['button_wishlist']       = 'Favoritos';
+$_['button_checkout']       = 'Pagar';
+$_['button_confirm']        = 'Confirmar pedido';
+$_['button_coupon']         = 'Aplicar Cupón';
+$_['button_delete']         = 'Borrar';
+$_['button_download']       = 'Descargar';
+$_['button_edit']           = 'Editar';
+$_['button_filter']         = 'Procura avanzada';
+$_['button_new_address']    = 'Engadir novo enderezo';
+$_['button_change_address'] = 'Cambiar o enderezo';
+$_['button_reviews']        = 'Opinións';
+$_['button_write']          = 'Engadir un comentario';
+$_['button_login']          = 'Acceder';
+$_['button_update']         = 'Actualizar';
+$_['button_remove']         = 'Eliminar';
+$_['button_reorder']        = 'Reordenar';
+$_['button_return']         = 'Devolver';
+$_['button_shopping']       = 'Continuar comprando';
+$_['button_search']         = 'Procurar';
+$_['button_shipping']       = 'Aplicar o envío';
+$_['button_submit']         = 'Enviar';
+$_['button_guest']          = 'Compra como invitado';
+$_['button_view']           = 'Ver';
+$_['button_voucher']        = 'Aplicar Vale';
+$_['button_upload']         = 'Subir';
+$_['button_reward']         = 'Aplicar puntos';
+$_['button_quote']          = 'Calcular';
+$_['button_list']           = 'Lista';
+$_['button_grid']           = 'Grella';
+$_['button_map']            = 'Ver Google Map';
+
+// Error
+$_['error_exception']       = 'Código de error(%s): %s en %s na liña %s';
+$_['error_upload_1']        = 'Advertencia: O arquivo subido excede o tamaño máximo permitido! (upload_max_filesize en php.ini)';
+$_['error_upload_2']        = 'Advertencia: O arquivo subido excede o tamaño máximo permitido para o formulario! (MAX_FILE_SIZE no formulario HTML)';
+$_['error_upload_3']        = 'Advertencia: O arquivo subiu só parcialmente!';
+$_['error_upload_4']        = 'Advertencia: O arquivo non foi subido!';
+$_['error_upload_6']        = 'Aviso: Falta unha carpeta temporal!';
+$_['error_upload_7']        = 'Advertencia: Non se puido escribir o arquivo no disco!';
+$_['error_upload_8']        = 'Advertencia: A carga de arquivos detívose, debido ao tipo de extensión!';
+$_['error_upload_999']      = 'Advertencia: Erro non identificado!';
+$_['error_curl']            = 'CURL: Código de Error(%s): %s';
+
+// Datepicker
+$_['datepicker']            = 'gl';
