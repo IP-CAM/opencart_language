74ad6a2ae16a8dd744ae4e29dd7604ed62d220b8
Emilio J. Padrón González <emilioj@udc.gal>
2019-11-20 13:38:27 +0100

    [English] Add strings for X-Discount/Special Price module (admin)

---
+++ english_pv/upload/admin/language/en-gb/extension/module/xdiscount.php
@@ -0,0 +1,177 @@
+<?php
+// Heading
+$_['heading_title']    = 'X-Discount';
+$_['button_save_continue'] = 'Save and Stay';
+
+// Common accors mods
+$_['text_for_all']    = 'For Any';
+$_['text_checked_all'] = 'Check all';
+$_['text_unchecked_all'] = 'Uncheck all';
+$_['text_batch_select']= 'Batch Selection';
+$_['text_remove_all']    = 'Remove All';
+$_['text_free_search']    = 'Type to search';
+$_['text_start']    = 'Start';
+$_['text_end']    = 'End';
+
+// Format: prefix_varname i.e entry_name or help_name
+// Entry
+$_['entry_display'] = 'Name';
+$_['entry_status']     = 'Status:';
+$_['entry_store'] = 'Store:';
+$_['entry_geo_zone']   = 'Geo Zone:';
+$_['entry_customer_group'] = 'Customer Group:';
+$_['entry_manufacturer_rule'] = 'Manufacturer Rule:';
+$_['entry_category'] = 'Category Rule';
+$_['entry_customer_group'] = 'Customer Group:';
+$_['entry_manufacturer'] = 'Manufacturer:';
+$_['entry_product_product']     = 'Products';
+$_['entry_product_category']   = 'Categories';
+$_['entry_days']    = 'Days of Week';
+$_['entry_time']    = 'Time Period';
+$_['entry_product'] = 'Product Rule';
+$_['entry_date']    = 'Date Range';
+$_['entry_date_start']    = 'Any Date';
+$_['entry_date_end']    = 'Any Date';
+$_['entry_int_data'] = 'Data(s)';
+$_['entry_cost']    = 'Discount Amount';
+$_['entry_unit_range']    = 'Discount Details';
+$_['entry_debug']    = 'Debugging:';
+$_['entry_module_status']    = 'Module Status';
+$_['entry_disable_default']    = 'Disable Default Product Promotions';
+$_['entry_special_page']    = 'Special Products Page';
+$_['entry_countdown']    = 'Show Countdown';
+$_['entry_ribbon']    = 'Show Ribbon';
+$_['entry_selector_ribbon_detail']    = 'Selector for ribbon';
+$_['entry_selector_countdown_detail']    = 'Selector for countdown';
+$_['entry_selector_product_box']    = 'Selector for Product Box';
+$_['entry_selector_ribbon_list']    = 'Selector for ribbon';
+$_['entry_selector_countdown_list']    = 'Selector for countdown';
+$_['entry_custom_css']    = 'Custom CSS';
+$_['entry_discount_type']    = 'Discount Type';
+$_['entry_special_type']    = 'Special Price By';
+$_['entry_keep_default']    = 'Keep existing special price';
+$_['entry_option_price']    = 'Discount on Option Price';
+$_['entry_show_discount_table']  = 'Display Discount Table';
+$_['entry_max']    = 'Maximum Discount Amount';
+$_['entry_final']    = 'Final Discount Apply On';
+$_['entry_total_type']    = 'Order Total Module Type';
+$_['entry_total_special']    = 'Special Module Title';
+$_['entry_total_discount']    = 'Discount Module Title';
+$_['entry_total_unified']    = 'Together Module Title';
+$_['entry_total_sort']    = 'Sort Order of Total Module';
+$_['entry_total_tax']    = 'Apply Discount on Taxes also';
+$_['entry_ribbon_format']    = 'Ribbon Text Format';
+$_['entry_ribbon_type']    = 'Ribbon Text Type';
+
+// Tabs
+$_['tab_help']    = 'Help & Supprt';
+$_['tab_global']    = 'Global Setting';
+$_['tab_global_general']    = 'General';
+$_['tab_general']    = 'General';
+$_['tab_price_setting']    = 'Price Setting';
+$_['tab_ribbon_setting']  = 'Ribbon & Countdown';
+$_['tab_criteria_setting']    = 'Generic Rules';
+$_['tab_category_product']    = 'Product Rules';
+$_['tab_final_discount']    = 'Cart Discount';
+$_['tab_text']    = 'Text Setting';
+
+// Text 
+$_['text_store_default'] = 'Default';
+$_['text_extension']  = 'Extensions';
+$_['text_edit']    = 'Configure Your X-Discount';
+$_['text_add_new_method']    = 'Add New Discount';
+$_['text_select_all']    = 'Select All';
+$_['text_unselect_all']    = 'Unselect All';
+$_['text_checall']    = 'For All';
+$_['text_any'] = 'For any';
+$_['text_ones_any'] = 'Any of the selected ones';
+$_['text_ones_except'] = 'Except the selected ones';
+$_['text_sunday']    = 'Sunday';
+$_['text_monday']    = 'Monday';
+$_['text_tuesday']    = 'Tuesday';
+$_['text_wednesday']    = 'Wednesday';
+$_['text_thursday']    = 'Thursday';
+$_['text_friday']    = 'Friday';
+$_['text_saturday']    = 'Saturday';
+$_['text_sub_total']     = 'SubTotal';
+$_['text_method_remove']    = 'Remove Discount';
+$_['text_method_copy']    = 'Copy Discount';
+$_['text_method_save']    = 'Save Discount';
+$_['text_help']    = 'Help';
+$_['text_guest_checkout']    = 'Guest Checkout';
+$_['text_delete_all']    = 'Delete All';
+$_['text_start']    = 'Start';
+$_['text_end']    = 'End';
+$_['text_cost']    = 'Cost';
+$_['text_add_new']    = 'Add New';
+$_['text_days_week']    = 'Days of Week';
+$_['text_time_period']    = 'Time Period';
+$_['text_rate_flat']    = 'Flat Amount';
+$_['text_no_unit_row']    = 'There is nothing to show, please click `Add New` button to add!';
+$_['text_remove']    = 'Remove';
+$_['text_sort_manual']    = 'Manual';
+$_['text_action'] = 'Action';
+$_['text_disable_default']    = 'Disable existing default Discount & Special Price';
+$_['text_special_page']    = 'Show X-Discounted Products on the special page instead of default products';
+$_['text_selector_detail']  = 'Product detail page';
+$_['text_selector_list']  = 'Product listing page';
+$_['text_page_detail']  = 'Product Page';
+$_['text_page_list']  = 'Listing Page';
+$_['text_discount_special']    = 'Special Price';
+$_['text_discount_discount']    = 'Discount By Quantity';
+$_['text_product_price']    = 'Product Price';
+$_['text_final_cart']    = 'Apply on Cart Product';
+$_['text_final_total']    = 'Display on Order Total Section';
+$_['text_final_total_apart'] = 'Quantity and Special Discount Separately';
+$_['text_final_total_unified'] = 'All Discounts Together';
+$_['text_total_tax']    = 'It will give discount on applicable taxes as well';
+$_['text_ribbon_percent']    = 'Percentage Value';
+$_['text_ribbon_flat']    = 'Flat Value';
+$_['text_ribbon_specific']    = 'Based on discount type';
+
+// Placeholder 
+$_['placeholder_cost'] = 'e.g. 10% or -10% or 10 or -10';
+$_['placeholder_custom_css'] = 'e.g .xdiscount_ribbon {background:#2f96c9;}';
+$_['placeholder_ribbon_format'] = 'e.g. %s OFF';
+
+// More Help
+
+// Help
+$_['help_display'] = 'For admin use only. It helps you to differentiate a discount from others.';
+$_['help_status']       = 'Enable/Disable this particular discount only';
+$_['help_store'] = $_['help_customer_group'] = $_['help_days'] = 'If you want to specify any particular items, please uncheck `For Any` to show the available options';
+$_['help_category'] = $_['help_product'] =  $_['help_manufacturer_rule']  = '<b>For any</b>: Valid for any items.<br /><b>Any of the selected ones</b>: Valid for the selected items<br /><b>Except the selected ones</b>: For any items except the selected ones';
+$_['help_time']       = 'Please set time period for this fee/discount. Note: Server time will be considered. Server time now: %s';
+$_['help_date']    = 'If you want to restrict fee/discount by date range, please choose applicable dates. Note: Server date will be considered. Server Date: %s';
+$_['help_module_status']       = 'Enable/Disable X-Discount';
+
+$_['help_unit_start']       = 'Counting will start from this value';
+$_['help_unit_end']       = 'Counting will end in this value';
+$_['help_unit_price']       = 'Discount Amount. Percentage is allowed e.g. 5 or 7% etc.';
+$_['help_cost'] = 'Percentage is allowed. For discount, please enter value with negative sign. e.g -10% Or 10% or 10';
+$_['help_custom_css']    = 'Don\'t put inside style tag. Just enter your css rule. e.g .xdiscount_ribbon {background:#2f96c9;}';
+$_['help_disable_default']    = 'This option will disable default special and discount price for all products';
+$_['help_special_page']    = 'By Default X-Discounted products appears along with opencart default special products. If you chose this option, only X-Discounted products would show on the special page';
+$_['help_countdown']    = 'Select the pages where you want to display discount countdown';
+$_['help_ribbon']    = 'Select the pages where you want to display ribbon';
+$_['help_selector_detail']  = 'Please enter CSS selectors how X-Discount will find the location to put in ribbon and countdown if you want to enable those features. To know how to find selectors, please check <a target="_blank" href="http://blog.opencartmart.com/how-to-select-css-selector-for-discount-ribbon-and-offer-countdown-in-x-discount/">this post</a>';
+$_['help_discount_type'] = 'Define what kind of discount you want to offer.';
+$_['help_special_type']  = '<b>Flat</b>: Any Flat value e.g 5 or 10%.<br /><b>Quantity</b>: Discount by product quantity.<br /><b>Product Price</b>: Discount by product price.';
+$_['help_cost']       = 'Discount Amount. Percentage is allowed. e.g. 5%, 10.99 etc.';
+$_['help_max']  = 'Limit your maximum discounted amount. You can define maximum applicable discount here. Mostly usable when the discount amount is percentage.';
+$_['help_show_discount_table']  = 'Show discount table on the product detail page';
+$_['help_keep_default']    = 'Don\'t overwrite existing special price. It will respect the product rules.';
+$_['help_option_price']    = 'Apply discounted price on Option Price also';
+$_['help_selector_rule']  = 'You can modify inserting mode using position text e.g. #content:after Or #content:before Or #content:append Or #content:prepend. Default mode is append';
+$_['help_final'] = 'Define how do you want to display final discount on the shopping cart.';
+$_['help_total_type']    = 'Define how do you want to show discount on the total section.';
+$_['help_total_text']    = 'Following fields are needed if you want to show final discount on the order total section';
+$_['help_total_sort']    = 'Define the postion you want to show the order total module. By default it will show up after the Sub Total';
+$_['help_ribbon_format']    = 'Define how the ribbon text would display';
+
+//more 
+$_['more_ribbon_format']    = '%s will be replace with discount amount.';
+
+// Error & Success
+$_['text_success']     = 'Success: You have modified X-Discount!';
+$_['error_permission'] = 'Warning: You do not have permission to modify X-Discount!';
