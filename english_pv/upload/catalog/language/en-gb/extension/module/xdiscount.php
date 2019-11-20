56c19da48f6bd66154bb41094a0a33b675dc8a1e
Emilio J. Padrón González <emilioj@udc.gal>
2019-11-20 13:22:20 +0100

    [English] [Spanish] Add strings for X-Discount/Special Price module

---
+++ english_pv/upload/catalog/language/en-gb/extension/module/xdiscount.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_offer_expire'] = 'Offers Ending in';
+$_['text_ribbon_format'] = '%s OFF'; //keep %s to replace discount amount
+$_['text_offer_day'] = 'Days';
+$_['text_offer_hour'] = 'Hours';
+$_['text_offer_minute'] = 'Minutes';
+$_['text_offer_second'] = 'Seconds';
+$_['text_day_initial'] = 'D';
+$_['text_hour_initial'] = 'H';
+$_['text_minute_initial'] = 'M';
+$_['text_second_initial'] = 'S';
